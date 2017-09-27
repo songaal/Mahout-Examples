85edec2f7edba6c70d964d6a27ebb2b781d287e5
Sang Song <songaal@gmail.com>
2017-09-28 08:01:18 +0900

    아이템 기반.

---
+++ quickstart/src/main/java/kr/swsong/recom/ItemRecommend.java
@@ -0,0 +1,61 @@
+package kr.swsong.recom;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.ItemBasedRecommender;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by swsong on 17. 9. 28..
+ */
+public class ItemRecommend {
+
+    public void process() throws IOException, TasteException {
+        String fileName = "ratings.csv";
+        DataModel dm = getDataModel(fileName);
+        //유사도 모델
+        ItemSimilarity sim = new LogLikelihoodSimilarity(dm);
+        //사용자 추천기.
+        ItemBasedRecommender recommender = new GenericItemBasedRecommender(dm, sim);
+
+        int x = 1;
+
+        int requestItemSize = 5;
+        LongPrimitiveIterator iterator = dm.getUserIDs();
+        while(iterator.hasNext()) {
+            long userId = iterator.nextLong();
+            //현재 유저 id에 해당하는 5개 아이템 추천.
+            List<RecommendedItem> itemList = recommender.recommend(userId, requestItemSize);
+//            logger.info("{}] {} =========", x++, userId);
+            System.out.println(x++ +"] " + userId + "=============");
+            for(RecommendedItem item : itemList) {
+//                logger.info("> {} : {}", item.getItemID(), item.getValue());
+                System.out.println("> " + item.getItemID() + " : " + item.getValue());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new ItemRecommend().process();
+    }
+
+    private DataModel getDataModel(String fileName) throws IOException {
+        String filePath = getClass().getClassLoader().getResource(fileName).getFile();
+        DataModel dm = new FileDataModel(new File(filePath));
+        return dm;
+    }
+}
